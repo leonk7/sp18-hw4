555cfffdd70907542bb0903cbe55db660927fb63
Leon Kwak <lkwak783@gmail.com>
2018-03-08 18:20:04 -0800

    this was too damn long

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,20 @@
+class TodosController < ApplicationController
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path
+  end
+
+  def index
+    @todos = Todos.all
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:tasks, :finished)
+  end
+end
---
+++ app/models/cat.rb
@@ -0,0 +1,5 @@
+class Cat < ApplicationRecord#ctiveRecord::Migration[5.1]
+	def meow
+		puts "meow"
+	end
+end
